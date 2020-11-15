725359ca329b14e6e8da0eb5dcc9787ea54cdcdc
Nicholas Reid <nicholasreid845@gmail.com>
2020-11-14 22:10:45 -0500

    rendering home html dynamically, added store object

--- index.js
+++ index.js
@@ -1,10 +1,50 @@
+let store = {
+    searchTerms: [],
+    searching: false
+}
 
 
-let searchTerms = [];
+
+
+
+
+/**HTML FUNCTIONS*/
+function returnHomePage() {
+    return `
+    <div class="hero-container">
+          <img src="" alt="hero-image">
+        </div>
+        <h2>Make a list of ingredients to search for recipes, select styles of cuisine as well</h2>
+        <div class="search-display hidden">
+
+        </div>
+        <form action=''>
+          <div>
+            <label for="search-term">Add ingredients below:</label>
+            <input type="text" name="search-term" id="search-term" placeholder="ex: garlic, italian">
+            <input type="button" name="add-item" id="add-item" value="ADD ITEM">
+            <input type="submit" id="search-submit">
+          </div>
+        </form>
+    `
+}
+
+
+
+
+/*********RENDER FUNCTION **************/
+function render() {
+    if (store.searching === false) {
+        $('main').append(returnHomePage())
+    }
+}
+
+
+
 function addItem() {
     $('main').on('click', '#add-item', function(event) {
-        searchTerms.push($('#search-term').val())
-        console.log(searchTerms)
+        store.searchTerms.push($('#search-term').val())
+        console.log(store.searchTerms)
     })
 }
 
@@ -15,6 +55,7 @@ function addItem() {
 
 function runFunctions() {
     $(addItem)
+    $(render)
 }
 
 
